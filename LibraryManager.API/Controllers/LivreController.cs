4649943a33ef63d0f830249f0f9a5035f4d056d8
Math Peeters <pmathieu.peeters@gmail.com>
2026-01-25 16:00:47 +0100

    Ajout d'une premiere route

--- LibraryManager.API/Controllers/LivreController.cs
+++ LibraryManager.API/Controllers/LivreController.cs
@@ -1,4 +1,7 @@
-﻿using LibraryManager.Core.Interfaces.Repositories;
+﻿using LibraryManager.API.DTOs.Responces;
+using LibraryManager.Core.Interfaces.Repositories;
+using LibraryManager.Core.Interfaces.Services;
+using LibraryManager.Domain.Entities;
 using Microsoft.AspNetCore.Mvc;
 
 namespace LibraryManager.API.Controllers;
@@ -8,5 +11,20 @@ namespace LibraryManager.API.Controllers;
 
 public class LivreController : ControllerBase
 {
-  private ILivreRepository _livreRepository;
+  private ILivreService? _livreService;
+
+  public LivreController(ILivreService livreService)
+  {
+    _livreService = livreService;
+  }
+
+  [HttpGet]
+  public async Task<ActionResult<IEnumerable<LivreResponceDto>>> GetLivres()
+  {
+    var livres = await _livreService.GetAllAsync();
+    return Ok(livres);
+  }
 }
+
+
+
---
+++ LibraryManager.API/DTOs/Responces/LivreResponceDto.cs
@@ -0,0 +1,8 @@
+﻿namespace LibraryManager.API.DTOs.Responces;
+
+public class LivreResponceDto
+{
+  public Guid Id { get; set; }
+  public required string Nom { get; set; } 
+  public required string Auteur { get; set; }
+}
